463a2af30d10d846c253845a8b4815aa2ee57d4e
Edison Wang <ew040607@gmail.com>
2026-08-05 21:43:55 -0400

    feat: env var pulling

--- backend/src/backboard.ts
+++ backend/src/backboard.ts
@@ -1,15 +1,17 @@
 import { BackboardClient } from 'backboard-sdk';
 
 export default class BackboardWrapper {
+  private readonly assistantID: string;
   private readonly client: BackboardClient;
 
-  private getAPIKey() {
-    const APIKey = process.env.BACKBOARD_API_KEY;
-    if (!APIKey) throw new Error('Backboard.io API key missing from .env');
+  private getEnvValue(key: string) {
+    const APIKey = process.env[key];
+    if (!APIKey) throw new Error(`${key} missing from .env`);
     return APIKey;
   }
 
   constructor() {
-    this.client = new BackboardClient({ apiKey: this.getAPIKey() });
+    this.client = new BackboardClient({ apiKey: this.getEnvValue("BACKBOARD_API_KEY") });
+    this.assistantID = this.getEnvValue("BACKBOARD_ASSISTANT_ID");
   }
 }
